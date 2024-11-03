f57183223449e79a7900d3f666f31608a484a068
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-11-03 16:13:24 +0100

    import funkcji modelu do kontrolera, pobranie tasków i wyrenderowanie widoku

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from model import add_task, get_all_tasks
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    tasks = get_all_tasks()
+    return render_template('index.html', tasks=tasks)
 
 
 if __name__ == '__main__':
