d0a0ee8b1fd1e0c343644e9bff48edd2e9945fbb
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-11-03 16:28:42 +0100

    zmodyfikowanie kontrolera, tak by akceptował zarówno metodę GET jak i POST; dodanie logiki obsługi formularza

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ from model import add_task, get_all_tasks
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    tasks = get_all_tasks()
-    return render_template('index.html', tasks=tasks)
+    if request.method == 'GET':
+        tasks = get_all_tasks()
+        return render_template('index.html', tasks=tasks)
+    elif request.method == 'POST':
+        new_task = request.form['task']
+        add_task(new_task)
+        return redirect(url_for('hello_world', _method='GET'))
 
 
 if __name__ == '__main__':
